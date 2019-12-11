09dbd1e0b363eb4301ecec25b665797545cc8f75
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-10 16:46:38 -0800

    add in mLab uri

--- models/index.js
+++ models/index.js
@@ -4,6 +4,11 @@ const dbUrl = process.env.MONGO_URI;
 
 
 mongoose.connect(dbUrl, {
+  if ( proccess.env.NODE_ENV == "PRODUCTION" ) {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+     mongoose.connect("mongodb://localhost:27017/chulo")
+  }
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
