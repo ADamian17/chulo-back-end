2d28e28e7d7df6b1b48f4979ea656f4bf81300f0
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-10 16:50:24 -0800

    adjust local vs remote conditional

--- models/index.js
+++ models/index.js
@@ -3,21 +3,19 @@ require('dotenv').config();
 const dbUrl = process.env.MONGO_URI;
 
 
-mongoose.connect(dbUrl, {
-  if ( proccess.env.NODE_ENV == "PRODUCTION" ) {
-    mongoose.connect(process.env.MLAB_URL)
-  } else {
-     mongoose.connect("mongodb://localhost:27017/chulo")
-  }
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected :)'))
-  .catch((err) => console.log(`MongoDB connection error: ${err}`));
 
-
+if ( proccess.env.NODE_ENV == "production" ) {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+    mongoose.connect("mongodb://localhost:27017/chulo",   
+    {useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true})
+    .then(() => console.log('MongoDB connected :)'))
+    .catch((err) => console.log(`MongoDB connection error: ${err}`));
+  
+}
 
 module.exports = {
   User: require('./User'),
